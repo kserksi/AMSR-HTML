8132ed76ba11b68bd39d3ec258ac104656fba206
kserksi <99547275+kserksi@users.noreply.github.com>
2024-07-11 16:48:25 +0800

    Add files via upload

---
+++ JS/cf.js
@@ -0,0 +1,4 @@
+<!-- Cloudflare Web Analytics -->
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js'
+        data-cf-beacon='{"token": "cc9a3ced161842db94d9b72030ad9d3a"}'></script>
+<!-- End Cloudflare Web Analytics -->
